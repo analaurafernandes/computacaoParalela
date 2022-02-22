35c12619e02aba65b00592d2bd344de86a064773
Ana Laura Fernandes <analaura.fernandes@dtidigital.com.br>
2022-02-22 13:38:33 -0300

    tarefa04

--- tarefa04.c
+++ tarefa04.c
@@ -1,11 +1,27 @@
+/*
+Aluna: Ana Laura Fernandes de Oliveira
+Matrícula: 680715
+Professor: Henrique Cota
+
+A diretiva pragma omp parallel for foi utilizada para que as iterações do loop 
+fossem divididas entre o grupo de threads geradas pela diretiva
+pragma omp parallel. 
+
+Tempo real sequencial: 2m43.728s
+Tempo real sequencial: 0m45.349s
+Speed-up: tempo sequencial/tempo paralelo = 3,61039935
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
 void mm(double* a, double* b, double* c, int width) 
 {
+ #pragma omp parallel for
  for (int i = 0; i < width; i++) {
+    #pragma omp parallel for
     for (int j = 0; j < width; j++) {
       double sum = 0;
+      #pragma omp parallel for
       for (int k = 0; k < width; k++) {
 	    double x = a[i * width + k];
 	    double y = b[k * width + j];
